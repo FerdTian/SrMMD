e13292c4c09d6f8d3e31967799bcbab0ea96acf2
Chenyang Tian <137880206+FerdTian@users.noreply.github.com>
2026-05-03 19:41:31 +0800

    Add files via upload

--- scripts/color_transfer.sh
+++ scripts/color_transfer.sh
@@ -2,7 +2,4 @@
 set -e
 cd "$(dirname "$0")/../src/generative"
 
-python color_transfer_new.py --out_dir ../../results/figure2_color_transfer --flow mmd --dataset cub --source_idx 7 --target_idx 1007 --split test --kernel gaussian --resize 256 --particles 8192 --step_num 3000 --step_size 0.01 --bandwidth 1.0 --lmbda 1e-2 --alpha 0.8 --metric_n 512 --log_every 25
-python color_transfer_new.py --out_dir ../../results/figure2_color_transfer --flow drmmd --dataset cub --source_idx 7 --target_idx 1007 --split test --kernel gaussian --resize 256 --particles 8192 --step_num 3000 --step_size 0.01 --bandwidth 1.0 --lmbda 1e-2 --alpha 0.8 --metric_n 512 --log_every 25
 python color_transfer_new.py --out_dir ../../results/figure2_color_transfer --flow srmmd --dataset cub --source_idx 7 --target_idx 1007 --split test --kernel gaussian --resize 256 --particles 8192 --step_num 3000 --step_size 0.01 --bandwidth 1.0 --lmbda 1e-2 --alpha 0.8 --metric_n 512 --log_every 25
-python color_transfer_new.py --out_dir ../../results/figure2_color_transfer --flow hrmmd --dataset cub --source_idx 7 --target_idx 1007 --split test --kernel gaussian --resize 256 --particles 8192 --step_num 3000 --step_size 0.01 --bandwidth 1.0 --lmbda 1e-2 --alpha 0.8 --metric_n 512 --log_every 25
--- scripts/sampling.sh
+++ scripts/sampling.sh
@@ -2,8 +2,4 @@
 set -e
 cd "$(dirname "$0")/../src/sampling"
 
-python main.py --seed 42 --method mmd --kernel Stein --particle_num 500 --lmbda 0.5 --alpha 0.8 --dataset mog --step_num 1000 --optimizer lbfgs --step_size 0.1 --bandwidth 0.3 --save_path ../../results/figure3_sampling_mog/
-python main.py --seed 42 --method srmmd --kernel Stein --particle_num 500 --lmbda 0.5 --alpha 0.8 --dataset mog --step_num 1000 --optimizer lbfgs --step_size 0.1 --bandwidth 0.3 --save_path ../../results/figure3_sampling_mog/
-python main.py --seed 42 --method hrmmd --kernel Stein --particle_num 500 --lmbda 0.5 --alpha 0.8 --dataset mog --step_num 1000 --optimizer lbfgs --step_size 0.1 --bandwidth 0.3 --save_path ../../results/figure3_sampling_mog/
-python main_svgd.py --seed 42 --method svgd --kernel Gaussian --dataset mog --particle_num 500 --step_num 1000 --step_size 0.01 --bandwidth 0.3 --svgd_bandwidth 0.3 --save_trajectory --save_path ../../results/figure3_sampling_mog/
-python main_svgd.py --seed 42 --method rsvgd --kernel Gaussian --dataset mog --particle_num 500 --step_num 1000 --step_size 0.01 --bandwidth 0.3 --svgd_bandwidth 0.3 --nu 0.01 --save_trajectory --save_path ../../results/figure3_sampling_mog/
+python main.py --seed 42 --method srmmd --kernel Stein --particle_num 500 --lmbda 0.5 --alpha 0.8 --dataset mog --step_num 1000 --optimizer sgd --step_size 0.1 --bandwidth 0.3 --save_path ../../results/figure3_sampling_mog/
