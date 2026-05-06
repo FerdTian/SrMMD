44163bc70c6a72cf50b9d5acfc9d77b0cfd15b46
Chenyang Tian <137880206+FerdTian@users.noreply.github.com>
2026-05-06 16:22:31 +0800

    Update toys.sh

--- scripts/toys.sh
+++ scripts/toys.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")/../src/generative"
 
 # Mixture of Gaussians
 
-python run.py --dataset MixGaussian --flow srmmd --kernel Gaussian --lmbda 0.1 --alpha 0.8 --step_size 0.1 --bandwidth 1.0 --step_num 4000 --source_particle_num 500 --target_particle_num 500 --opt sgd --seed 42 --offset 0 --logccv 3.0 --save_path ../../results/figure1_toys/
+python run.py --dataset MixGaussian --flow srmmd --kernel Gaussian --lmbda 0.1 --alpha 0.8 --step_size 0.1 --bandwidth 1.0 --step_num 4000 --source_particle_num 500 --target_particle_num 500 --opt sgd --seed 42 --save_path ../../results/figure1_toys/
 
 # Swiss Roll
 
-python run.py --dataset swissroll --flow srmmd --kernel Energy --lmbda 0.1 --alpha 0.8 --step_size 0.1 --bandwidth 1.0 --step_num 4000 --source_particle_num 500 --target_particle_num 500 --opt sgd --seed 42 --offset 0 --logccv 3.0 --save_path ../../results/figure1_toys/
+python run.py --dataset swissroll --flow srmmd --kernel Energy --lmbda 0.1 --alpha 0.8 --step_size 0.1 --bandwidth 1.0 --step_num 4000 --source_particle_num 500 --target_particle_num 500 --opt sgd --seed 42 --save_path ../../results/figure1_toys/
