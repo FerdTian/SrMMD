b47fa6ab14661b4bcca7d5ef5a721ce4ef8e8297
Chenyang Tian <137880206+FerdTian@users.noreply.github.com>
2026-05-06 16:22:00 +0800

    Update student_teacher.sh

--- scripts/student_teacher.sh
+++ scripts/student_teacher.sh
@@ -2,4 +2,4 @@
 set -e
 cd "$(dirname "$0")/../src/generative"
 
-python student_teacher/train.py --device -1 --lmbda 0.1 --loss srmmd --lr 1.0 --with_noise false --noise_decay_freq 500 --seed 42 --log_in_file --num_particles 100 --N_train 1000 --N_valid 1000 --total_epochs 15000 --log_dir ../../results/figure2_student_teacher/
+python student_teacher/train.py --device -1 --lmbda 0.1 --loss srmmd --lr 0.1 --with_noise false --noise_decay_freq 500 --seed 42 --log_in_file --num_particles 100 --N_train 1000 --N_valid 1000 --total_epochs 15000 --log_dir ../../results/figure2_student_teacher/
